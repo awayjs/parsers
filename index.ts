0e1a30733ed24d282ebeaf3b2f21428551352046
80prozent <80prozent@differentdesign.de>
2018-06-18 11:18:18 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - Parsers - 0.6.45");
+console.log("AwayJS - Parsers - 0.6.46");
 
 export {DefaultSceneGraphFactory} from "./lib/factories/DefaultSceneGraphFactory";
 export {AWDParser} from "./lib/AWDParser";
